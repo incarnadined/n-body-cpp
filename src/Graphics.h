4e28f76f90e5af89b149196f1fada3257772e9f0
incarnadined <51889539+incarnadined@users.noreply.github.com>
2021-09-22 20:11:02 +0100

    Revert "added temporary skybox images and initial code"
    This reverts commit 31ec5b57f0c63ac8848d2e912767cc3f4034e6c2.

--- src/Graphics.h
+++ src/Graphics.h
@@ -48,8 +48,6 @@ public:
 	static void GenerateSphereMesh(int depth); // populates mesh with vertex and index array for a sphere
 	std::pair<std::vector<Vertex>, std::vector<unsigned int>> GenerateSphere(float radius, Vec3f position, size_t offset); // returns vertex and index array for a sphere
 	void Draw(); 
-	void InitSkybox();
-	void DrawSkybox();
 	void DrawImGui();
 	void EndFrame();
 
@@ -74,14 +72,6 @@ private:
 	int currentDepth = 3;
 	Camera camera;
 	Clock clock;
-
-	// skybox
-	bool sInit;
-	std::vector<Vertex> sVerticies;
-	std::vector<unsigned int> sIndicies;
-	std::vector<std::vector<Colour>> sImages;
-	Microsoft::WRL::ComPtr<ID3D11Texture2D> pSkyboxTexture;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> pSkyboxTextureView;
 
 	// com objects
 	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwapChain;
